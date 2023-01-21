09eb403c10ff91d23da627594e20cf1337b746ca
vartex <vartexxx29@yandex.ru>
2023-01-22 00:46:43 +0500

    fix: исправления после 2-го ревью

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -48,7 +48,7 @@ class FollowViewSet(
     search_fields = ('=following__username',)
 
     def get_queryset(self):
-        return self.request.user.follower
+        return self.request.user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
